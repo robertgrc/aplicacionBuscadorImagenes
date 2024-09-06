122de3c26a5f357e0904c5b9fe8355ac95d0b256
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-09-06 18:41:02 -0400

    Creando los dos routers

---
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,26 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { Navbar } from "../../ui"
+import { DcPage, MarvelPage, SearchPage,HeroPage} from "../pages"
+
+
+
+export const HeroesRoutes = () => {
+  return (
+    <>
+        <Navbar />
+
+        <div className="container">
+          <Routes>
+              <Route path="marvel" element={<MarvelPage/>}/>
+              <Route path="dc" element={<DcPage/>}/>
+              <Route path="search" element={<SearchPage/>}/>
+              <Route path="hero" element={<HeroPage/>}/>
+
+  
+              {/* Search, Hero by id */}
+              <Route path="/" element={<Navigate to="/marvel" />} />
+          </Routes>
+        </div>
+    </>
+  )
+}
