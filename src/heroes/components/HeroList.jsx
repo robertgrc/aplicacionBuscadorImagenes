0bea810760fab6b520aab86b4dd7bad08b8128dd
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-09-09 19:13:41 -0400

    Utilizando useMemo en HeroCard y HeroLis

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,11 +1,12 @@
+import { useMemo } from "react"
 import { getHeroesByPublisher } from "../helpers"
 import { HeroCard } from "./HeroCard"
 
 
 export const HeroList = ({ publisher }) => {
 
-    const heroes = getHeroesByPublisher( publisher )
-// console.log(heroes)
+    const heroes = useMemo (()=>getHeroesByPublisher( publisher ), [publisher])
+
   return (
     <div className="row rows-cols-1 row-cols-md-3 g-3">
         {heroes.map((hero)=>( 
