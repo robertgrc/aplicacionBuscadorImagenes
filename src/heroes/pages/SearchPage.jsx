b22cb8b8efb1f40454958bf879f2583b18d2bb98
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-09-09 23:18:44 -0400

    Diseno de la pantalla de busqueda

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,7 +1,41 @@
+import { HeroCard } from "../components"
 
 
 export const SearchPage = () => {
   return (
-    <h1>searchPAge</h1>
+    <>
+     <h1>Search</h1>
+     <hr/>
+
+    <div className="row">
+      <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+          <form action="">
+              <input 
+                type="text" 
+                placeholder="Search a Hero"
+                className="form-control"
+                name="searchText"
+                autoComplete="off"
+              />
+          </form>
+          <button className="btn btn-outline-primary mt-1">
+            Search
+          </button>    
+       </div>
+       <div className="col-7">
+            <h4>Results</h4>
+            <div className="alert alert-primary">
+              Search a Hero
+            </div>
+            <div className="alert alert-danger">
+              No hero <b>ABC</b>
+            </div>
+
+            {/* <HeroCard {...hero} /> */}
+          </div>
+    </div>
+    </> 
   )
 }
